6d7f72c89b90e1e3c735b3a0884e2811aa7459c3
joriwind <j.winderickx@gmail.com>
2017-05-29 17:14:40 +0200

    More

--- cisixlowpan/server.go
+++ cisixlowpan/server.go
@@ -1,12 +1,71 @@
 package cisixlowpan
 
 import "context"
+import "net"
+import "github.com/joriwind/hecomm-fog/hecomm"
+import "log"
 
 //Server Object defining the server
 type Server struct {
+	ctx     context.Context
+	comlink chan Message
+	address net.UDPAddr
+	Nodes   []Node
 }
 
 //NewServer create new server
-func NewServer(ctx context.Context, comlink chan Message) {
+func NewServer(ctx context.Context, comlink chan Message, host net.UDPAddr) *Server {
+	return &Server{
+		ctx:     ctx,
+		comlink: comlink,
+		address: host,
+	}
+}
 
+type packet struct {
+	Addr net.UDPAddr
+	Data []byte
+}
+
+//Node Connected node information
+type Node struct {
+	Addr   net.UDPAddr
+	Link   hecomm.LinkContract
+	OsSKey [16]byte
+	//AppSKey & NwkSKey managed by slip
+
+}
+
+//Start Start listening on configured UDP address
+func (s *Server) Start() error {
+	ln, err := net.ListenUDP("udp6", &s.address)
+	if err != nil {
+		return err
+	}
+	defer ln.Close()
+
+	log.Printf("Startin UDP server on %v\n", &s.address)
+
+	buf := make([]byte, 1024)
+
+	for {
+		n, addr, err := ln.ReadFromUDP(buf)
+		if err != nil {
+			return err
+		}
+
+		p := packet{Addr: *addr, Data: buf[:n]}
+		go handleUDPPacket(p)
+
+		select {
+		case <-s.ctx.Done():
+			return nil
+		default:
+
+		}
+	}
+}
+
+func handleUDPPacket(p packet) error {
+	return nil
 }
--- main.go
+++ main.go
@@ -1,12 +1,115 @@
 package main
 
 import (
+	"bufio"
 	"context"
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
 
+	"net"
+
+	"log"
+
+	coap "github.com/dustin/go-coap"
 	"github.com/joriwind/hecomm-6lowpan/cisixlowpan"
 )
 
 func main() {
+
+	//Flag init
+	flag.Usage = func() {
+		fmt.Printf("Usage of %s:\n", os.Args[0])
+		fmt.Printf("-address")
+	}
+
+	//Resolve UDP server address
+	address := flag.String("address", "[::1]:5683", "Server address of UDP listener")
+	flag.Parse()
+	if "" == *address {
+		*address = "[::1]:5683"
+	}
+	srvAddress, err := net.ResolveUDPAddr("udp6", *address)
+	if err != nil {
+		log.Printf("Not valid UDP server address: %v, err = %v\n", *address, err)
+		return
+	}
+
+	//Create server context
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	//Starting 6LoWPAN server
 	channel := make(chan cisixlowpan.Message)
-	cisixlowpan.NewServer(context.Background(), channel)
+	server := cisixlowpan.NewServer(ctx, channel, *srvAddress)
+	go func() {
+		err := server.Start()
+		if err != nil {
+			log.Fatalf("Exited with error: %v", err)
+		} else {
+			log.Printf("Exited\n")
+		}
+	}()
+
+	//Start io input --> commands
+	//command line interface of hecomm-fog
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		if scanner.Scan() {
+			line := scanner.Text()
+			//Split line into 2 parts, the command and OPTIONALY data
+			command := strings.SplitN(line, " ", 2)
+			switch command[0] {
+
+			case "exit":
+				cancel()
+				return
+
+			case "send":
+				subcommand := strings.SplitN(command[1], " ", 4)
+				i, err := strconv.Atoi(subcommand[0])
+				if err != nil {
+					fmt.Printf("Error in conversion: %v", err)
+					break
+				}
+				code := coap.COAPCode(uint8(i))
+				sendCoapRequest(code, subcommand[1], subcommand[2], subcommand[3])
+
+			case "help":
+
+			case "":
+			default:
+				fmt.Printf("Did not understand command: %v\n", command[0])
+			}
+		}
+	}
+}
+
+func sendCoapRequest(code coap.COAPCode, destination string, path string, payload string) {
+	req := coap.Message{
+		Type:      coap.Confirmable,
+		Code:      coap.GET,
+		MessageID: 12345,
+		Payload:   []byte(payload),
+	}
+
+	req.SetOption(coap.ETag, "weetag")
+	req.SetOption(coap.MaxAge, 3)
+	req.SetPathString(path)
+
+	c, err := coap.Dial("udp", destination)
+	if err != nil {
+		log.Fatalf("Error dialing: %v", err)
+	}
+
+	rv, err := c.Send(req)
+	if err != nil {
+		log.Fatalf("Error sending request: %v", err)
+	}
+
+	if rv != nil {
+		log.Printf("Response payload: %s", rv.Payload)
+	}
 }
