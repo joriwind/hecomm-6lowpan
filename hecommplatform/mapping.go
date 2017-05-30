ec7a50e414d94dfd4af8ddc37f812590090b2429
joriwind <j.winderickx@gmail.com>
2017-05-30 15:50:16 +0200

    hecomm

---
+++ hecommplatform/mapping.go
@@ -0,0 +1,6 @@
+package hecommplatform
+
+//ConvertHecommDevEUIToPlatformDevEUI Converts the deveui to platform's specific identifier
+func ConvertHecommDevEUIToPlatformDevEUI(devEUI []byte) string {
+	return string(devEUI)
+}
